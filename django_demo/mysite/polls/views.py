72340eacc1e63869c16aeb345ffef24227125cd0
Nathan Pillai <natepill@gmail.com>
2019-06-05 15:14:03 -0700

    [added] 3 views

--- django_demo/mysite/polls/views.py
+++ django_demo/mysite/polls/views.py
@@ -6,3 +6,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Successful HttpResponse! This is polling index page!")
     # To call the view, we need to map it to a URL - and for this we need a URLconf.
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
