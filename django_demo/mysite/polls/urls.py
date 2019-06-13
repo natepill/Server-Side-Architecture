5570f75f4f756e07c96262885b36bf27defba6f7
Nathan Pillai <natepill@gmail.com>
2019-06-13 15:32:16 -0700

    [added] internal git repo and refactored vote function in views

--- django_demo/mysite/polls/urls.py
+++ django_demo/mysite/polls/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
