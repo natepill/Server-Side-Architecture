75ece98619ea35d85133cbe07a193d05b77c5acb
Nathan Pillai <natepill@gmail.com>
2019-06-13 16:00:24 -0700

    [added] Part 4

--- django_demo/mysite/polls/views.py
+++ django_demo/mysite/polls/views.py
@@ -5,28 +5,51 @@ from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.db.models import F, Func
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(request, 'polls/index.html', context)
-    # To call the view, we need to map it to a URL - and for this we need a URLconf.
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-
-    question = get_object_or_404(Question, pk= question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list
+#     }
+#
+#     return render(request, 'polls/index.html', context)
+#     # To call the view, we need to map it to a URL - and for this we need a URLconf.
+#
+# def detail(request, question_id):
+#
+#     question = get_object_or_404(Question, pk= question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/result.html', {'question' question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -40,6 +63,8 @@ def vote(request, question_id):
         })
     else:
         selected_choice.votes += 1
+        # Will prevent single thread being overloaded... kinda
+        # selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
