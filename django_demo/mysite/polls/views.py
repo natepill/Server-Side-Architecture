b61327661b84a1456282c90158fef39546cb3000
Nathan Pillai <natepill@gmail.com>
2019-06-12 17:57:03 -0700

    [added] Part 3

--- django_demo/mysite/polls/views.py
+++ django_demo/mysite/polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
-
+from django.http import Http404
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
@@ -12,11 +12,16 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+
+    return render(request, 'polls/index.html', context)
     # To call the view, we need to map it to a URL - and for this we need a URLconf.
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+
+    question = get_object_or_404(Question, pk= question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
